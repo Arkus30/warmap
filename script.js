45dab640bf563878019ea26da0a818a1c1e67821
Arkus30 <wilrikbozzolo1@gmail.com>
2026-03-13 15:42:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
 const sheetURL = "https://opensheet.elk.sh/15VmVU4c4awO3rbVCv2PgqpoZb_CT-nlXRDxzuqBAiiQ/Feuille%201";
 
+function loadSectors(data){
+
+    const layer = document.getElementById("sectors-layer");
+    layer.innerHTML = "";
+
+    const sectors = {};
+
+    data.forEach(planet => {
+
+        if(!planet.X || !planet.Y || !planet.Secteur) return;
+
+        const x = parseFloat(String(planet.X).replace(",", "."));
+        const y = parseFloat(String(planet.Y).replace(",", "."));
+
+        if(!sectors[planet.Secteur]){
+            sectors[planet.Secteur] = {
+                xTotal:0,
+                yTotal:0,
+                count:0,
+                faction:planet.Faction || "inconnue"
+            };
+        }
+
+        sectors[planet.Secteur].xTotal += x;
+        sectors[planet.Secteur].yTotal += y;
+        sectors[planet.Secteur].count++;
+
+    });
+
+    Object.values(sectors).forEach(sector => {
+
+        const x = sector.xTotal / sector.count;
+        const y = sector.yTotal / sector.count;
+
+        const div = document.createElement("div");
+
+        const factionClass = "faction-" + sector.faction.toLowerCase();
+
+        div.className = "sector " + factionClass;
+
+        div.style.left = (x * 100) + "%";
+        div.style.top = (y * 100) + "%";
+
+        div.style.width = "220px";
+        div.style.height = "220px";
+
+        layer.appendChild(div);
+
+    });
+
+}
+
 async function loadPlanets(){
 
     const response = await fetch(sheetURL);
     const data = await response.json();
+
+    loadSectors(data);
 
     console.log("Planets loaded:", data);
 
